35d26eeed679a0bc75abcf84d61090a47a42ffd9
s-brown01 <sbrown01@carroll.edu>
2026-03-15 14:23:34 -0600

    Making Tone read from a textfile instead of hard-coded Mary Had A Little Lamb

--- src/Tone.java
+++ src/Tone.java
@@ -2,23 +2,45 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 enum NoteLength {
-    WHOLE(1.0f),
-    HALF(0.5f),
-    QUARTER(0.25f),
-    EIGTH(0.125f);
+    WHOLE(1),
+    HALF(2),
+    QUARTER(4),
+    EIGHTH(8),
+    SIXTEENTH(16),
+    THIRTY_SECOND(32);
+    
+    private final int division;
     
     private final int timeMs;
     
-    NoteLength(float length) {
-        timeMs = (int) (length * Note.MEASURE_LENGTH_SEC * 1000);
+    NoteLength(int division) {
+        this.division = division;
+        timeMs = (int) (Note.MEASURE_LENGTH_SEC * 1000) / division;
     }
     
     public int timeMs() {
         return timeMs;
+    }
+    
+    public int division() {
+        return division;
+    }
+    
+    public static NoteLength fromDivision(int division) {
+        for (NoteLength nl : values()) {
+            if (nl.division == division) {
+                return nl;
+            }
+        }
+        return null;
     }
 }
 
@@ -74,7 +96,7 @@ enum Note {
 public class Tone {
     
     // Mary had a little lamb
-    private static final List<BellNote> song = new ArrayList<BellNote>() {{
+    private static final List<BellNote> MARY_LITTLE_LAMB = new ArrayList<BellNote>() {{
         add(new BellNote(Note.A5, NoteLength.QUARTER));
         add(new BellNote(Note.G4, NoteLength.QUARTER));
         add(new BellNote(Note.F4, NoteLength.QUARTER));
@@ -114,7 +136,11 @@ public class Tone {
         final AudioFormat af =
                 new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, false);
         Tone t = new Tone(af);
-        t.playSong(song);
+        List<BellNote> newSong = t.readSong();
+        t.playSong(newSong);
+        
+//        t.playSong(MARY_LITTLE_LAMB);
+    
     }
     
     private final AudioFormat af;
@@ -133,6 +159,61 @@ public class Tone {
             }
             line.drain();
         }
+    }
+    
+    private List<BellNote> readSong() {
+        final String filename = "res/songs/MaryLamb.txt";
+        final List<BellNote> song = new ArrayList<>();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line =reader.readLine()) != null) {
+                String[] note_length = line.split(" ");
+                
+                BellNote note = parseBellNote(note_length);
+                
+                if (note != null) {
+                    song.add(note);
+                }
+            }
+        
+        } catch (FileNotFoundException e) {
+            System.err.println("No song found with filename: " + filename);
+        } catch (IOException e) {
+            System.err.println("IO Exception has occurred with reading song: " + filename);
+        }
+        
+        return song;
+        
+    }
+    
+    private BellNote parseBellNote(String[] note_length) {
+        // should always be 2 objects in the array: Note and Length
+        // more or less than 2 and try the next line
+        if (note_length.length != 2) {
+            return null;
+        }
+        
+        // try to get a BellNote out of the given string array
+        try {
+            // try to get the note to play, should be in the Note enum
+            Note tempNote = Note.valueOf(note_length[0]);
+            // get the length of the note to play, should be an int
+            int lengthNum = Integer.parseInt(note_length[1]);
+            // using the new int, get the intended note's play length
+            NoteLength tempLength = NoteLength.fromDivision(lengthNum);
+            
+            // if the NoteLength is not valid, fromDivision returns null, so make sure it is not null
+            if (tempLength == null) {
+                return null;
+            }
+            
+            return new BellNote(tempNote, tempLength);
+        } catch (IllegalArgumentException e) {
+            // this is for Note.Value of and
+            return null;
+        }
+        
     }
     
     private void playNote(SourceDataLine line, BellNote bn) {
