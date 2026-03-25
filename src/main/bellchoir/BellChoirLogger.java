5569f49ed0035e347199340cd426da97ad03d087
s-brown01 <sbrown01@carroll.edu>
2026-03-25 16:52:00 -0600

    Changing BellChoirLogger so that logs at WARNING or higher will be printed to the console so the user can see them

--- src/main/bellchoir/BellChoirLogger.java
+++ src/main/bellchoir/BellChoirLogger.java
@@ -7,20 +7,33 @@ public class BellChoirLogger {
     
     public static Logger createLogger(String name) {
         Logger logger = Logger.getLogger(name);
-        logger.setUseParentHandlers(false); // prevent console output
+        // prevents duplicate logging
+        logger.setUseParentHandlers(false);
         
+        // Console handler for the console
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        // only WARNING and above for the console
+        // the user shouldn't see every message, just the ones that are bad
+        consoleHandler.setLevel(Level.WARNING);
+        consoleHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(consoleHandler);
+        
+        // File handler for logs/log.txt
         try {
-            // logs/log-yyyy-MM-dd.log
-            String date = new java.text.SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date());
-            FileHandler fh = new FileHandler("logs/log-" + date + ".log", true);
-            fh.setFormatter(new SimpleFormatter());
-            logger.addHandler(fh);
-            logger.setLevel(Level.INFO);
+            // this goes specifically to the logs directory
+            FileHandler fileHandler = new FileHandler("logs/log.txt", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            // everything goes to the file
+            // assuming if the user voluntarily looks at the logs file they want to see all logs
+            fileHandler.setLevel(Level.ALL);
+            logger.addHandler(fileHandler);
         } catch (IOException e) {
-            System.err.println("An error occurred while trying to create a Logger for " + name);
+            System.err.println("Logger could not open file, terminating program");
             System.exit(1);
         }
         
+        // Overall logger level
+        logger.setLevel(Level.ALL); // send everything to handlers
         return logger;
     }
 }
