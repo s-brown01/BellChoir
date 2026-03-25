1c8de7aab50e789899f7cee959561cee977c0aee
s-brown01 <sbrown01@carroll.edu>
2026-03-25 12:41:22 -0600

    Added the playSong functionality to the Conductor class, created a very simple functionality test in the main method (likely to be replaced with better tests) and changed the CreateChoir so each ChoirMember gets a SourceDataLine to play their note on

--- src/bellchoir/Conductor.java
+++ src/bellchoir/Conductor.java
@@ -1,96 +1,84 @@
 package bellchoir;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import java.util.*;
 
 public class Conductor implements Runnable {
     
-    private final List<BellNote> song;
-    private final Map<Note, ChoirMember> choir;
-    private final Thread thread;
-    private volatile boolean isRunning = true;
-    
     public static void main(String[] args) {
-        // Build a fake song
         List<BellNote> testSong = new ArrayList<>();
         testSong.add(new BellNote(Note.A4, NoteLength.QUARTER));
-        testSong.add(new BellNote(Note.C4, NoteLength.QUARTER));
-        testSong.add(new BellNote(Note.E4, NoteLength.QUARTER));
-        testSong.add(new BellNote(Note.A4, NoteLength.QUARTER));
+        testSong.add(new BellNote(Note.C4, NoteLength.SIXTEENTH));
         testSong.add(new BellNote(Note.REST, NoteLength.QUARTER));
+        testSong.add(new BellNote(Note.E4, NoteLength.THIRTY_SECOND));
+        testSong.add(new BellNote(Note.A4, NoteLength.EIGHTH));
+        testSong.add(new BellNote(Note.REST, NoteLength.QUARTER));
+        testSong.add(new BellNote(Note.E4, NoteLength.HALF));
+        testSong.add(new BellNote(Note.A4, NoteLength.QUARTER));
+        testSong.add(new BellNote(Note.REST, NoteLength.WHOLE));
+        testSong.add(new BellNote(Note.E4, NoteLength.EIGHTH));
+        testSong.add(new BellNote(Note.A4, NoteLength.WHOLE));
+        testSong.add(new BellNote(Note.REST, NoteLength.HALF));
         
-        // Create a conductor
-        Conductor conductor = new Conductor(testSong, "TestConductor");
+        AudioFormat af = new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, false);
+        
+        Conductor conductor = new Conductor(testSong, af, "TestConductor");
         
         conductor.start();
-        int i = 0;
-        while (i < 10) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.err.println("Interrupted Conductor while testing");
-                break;
-            }
-            System.out.print(".");
-            i++;
+        
+        try {
+            conductor.thread.join(); // wait for song to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println();
-        // Stop all members
-        conductor.stop();
         
         System.out.println("Test finished");
     }
     
-    /**
-     * Creates a Conductor with the name "Conductor".
-     */
-    public Conductor(List<BellNote> song) {
-        this(song, "Conductor");
-    }
+    private final List<BellNote> song;
+    private final Map<Note, ChoirMember> choir;
+    private final Thread thread;
+    private final AudioFormat af;
+    private volatile boolean isRunning = true;
     
     /**
-     * Creates a Conductor with the specified ConductorName.
-     * If no ConductorName is provided, the default is "Conductor"
+     * Creates a Conductor with the specified ConductorName. If no ConductorName is provided, the default is
+     * "Conductor"
      *
      * @param ConductorName the name for this Conductor
      */
-    public Conductor(List<BellNote> song, String ConductorName) {
+    public Conductor(List<BellNote> song, AudioFormat af, String ConductorName) {
         this.song = song;
+        this.af = af;
         this.choir = new HashMap<>();
         this.thread = new Thread(this, ConductorName);
         
-        this.createChoir();
-//        this.thread.start();
     }
     
-    private void createChoir() {
+    private void createChoir(SourceDataLine line) {
         final List<Note> allNotes = new LinkedList<>();
+        // set all the unique notes that will be played in the song
         for (BellNote bn : song) {
             if (!allNotes.contains(bn.note)) {
                 allNotes.add(bn.note);
             }
         }
-
-        for (int i = 0; i < allNotes.size(); i += 2) {
-            final Note note1 = allNotes.get(i);
-            final Note note2;
-            if (i + 1 < allNotes.size()) {
-                 note2 = allNotes.get(i + 1);
-            } else {
-                note2 = null;
-            }
-            
-            final ChoirMember newMember = new ChoirMember(note1, note2, "Member #" + (i + 1));
-            choir.put(note1, newMember);
-            if (note2 != null) {
-                choir.put(note2, newMember);
-            }
+        // for each unique note, make a ChoirMember who will play it
+        int i = 0;
+        for (Note note : allNotes) {
+            // i starts at 0 before loop, so incrementing here means the member numbering starts at 1
+            i++;
+            final ChoirMember newMember = new ChoirMember(note, line, "Member #" + (i));
+            choir.put(note, newMember);
         }
     }
     
     public void start() {
         isRunning = true;
         thread.start();
-//        startChoir();
     }
     
     public void stop() {
@@ -111,15 +99,49 @@ public class Conductor implements Runnable {
     public void run() {
         System.out.println("Conductor starting");
         startChoir();
-        
-        while (isRunning) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.err.println("Conductor interrupted");
+        // give the same SourceDataLine to all choir members
+        // create the choir once told to run
+        // it is a bit of a small delay till the song is played when thread is started, but worth it
+        try (final SourceDataLine line = AudioSystem.getSourceDataLine(af)) {
+            // open the line and start it
+            line.open();
+            line.start();
+            
+            // create the choir
+            createChoir(line);
+            
+            // play the song
+            for (BellNote bn : song) {
+                ChoirMember cm = choir.get(bn.note);
+                System.out.println("BN Note: " + bn.note.toString() + " played by member: " + cm.getName());
+                try {
+                    Thread.sleep(bn.length.timeMs());
+                } catch (InterruptedException e) {
+                    System.err.println("Conductor interrupted while sleeping");
+                    System.exit(1);
+                }
+                
+//                ChoirMember cm = choir.get(bn.note);
+//                if (cm != null) {
+//                    System.out.println("Conductor cueing " + cm.getName() + " for " + bn.note);
+//                    cm.play(bn);
+//                }
+//
+//                // wait for note duration
+//                Thread.sleep(bn.length.timeMs());
             }
+            
+            // finished song so shut down
+            stopChoir();
+            line.drain();
+            
+        } catch (LineUnavailableException e) {
+            System.err.println("An error occurred while trying to play the song, terminating program");
+            System.exit(1);
         }
-        System.out.println("Conductor stopping");
+        
+
+        System.out.println("Conductor ending");
 //        stopChoir();
     }
     
