99bb39d4ba2828a677b79c55bc5f8258f2082367
s-brown01 <sbrown01@carroll.edu>
2026-03-25 12:39:52 -0600

    Making each ChoirMember only have 1 note to play, and adding a SourceDataLine to the Constructor so each member knows where to play their note

--- src/bellchoir/ChoirMember.java
+++ src/bellchoir/ChoirMember.java
@@ -1,26 +1,28 @@
 package bellchoir;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class ChoirMember implements Runnable {
     
-    private final Note note1;
-    private final Note note2;
+    private final Note note;
     private final Thread thread;
+    private final SourceDataLine audio;
     private volatile boolean isPlaying;
+    private final boolean myTurn;
     
-    public ChoirMember(Note note1, Note note2, String name) {
-        this.note1 = note1;
-        this.note2 = note2;
+    public ChoirMember(Note note, SourceDataLine audio, String name) {
+        this.note = note;
+        this.audio = audio;
         this.isPlaying = false;
+        myTurn = false;
+        
         this.thread = new Thread(this, name);
     }
     
-    public Note getNote1() {
-        return note1;
+    public Note getNote() {
+        return note;
     }
     
-    public Note getNote2() {
-        return note2;
-    }
     
     public String getName() {
         return thread.getName();
@@ -39,6 +41,13 @@ public class ChoirMember implements Runnable {
         if (isPlaying) {
             System.out.println(thread.getName() + " started");
         }
+        synchronized (this) {
+            while (isPlaying) {
+                if (myTurn) {
+                
+                }
+            }
+        }
     }
     
     public void stop() {
@@ -49,8 +58,11 @@ public class ChoirMember implements Runnable {
         System.out.println(thread.getName() + " stopped");
     }
     
-    private void playNote(){
-        // play a note?
+    private void playNote(Note note, NoteLength length) {
+        final int ms = Math.min(length.timeMs(), Note.MEASURE_LENGTH_SEC * 1000);
+        final int length_time = Note.SAMPLE_RATE * ms / 1000;
+        audio.write(note.sample(), 0, length_time);
+        audio.write(Note.REST.sample(), 0, 50);
     }
     
 }
